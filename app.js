d2eaad5bd887c24624b12cc355ba07ec211eae96
ksbm009 <kevinstephenbiju2003@gmail.com>
2024-02-26 23:51:59 +0530

    Update MongoDB connection string

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const useragent = require("express-useragent");
 
 var user = require('./routes/userRoute');
 
-mongoose.connect(process.env.MONGOURL || 'mongodb+srv://kevinstephenbiju2025:<password>@ksbmcluster01.rfmkko1.mongodb.net/',
+mongoose.connect(process.env.MONGOURL || 'mongodb+srv://kevinstephenbiju2025:YgPEgzKU86t@ksbmcluster01.rfmkko1.mongodb.net/?retryWrites=true&w=majority&appName=KsbmCluster01',
 {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    console.log('Connected to Database');
+    console.log('Connected to Database!');
 }).catch((er)=>{
     console.log(er);
 });
