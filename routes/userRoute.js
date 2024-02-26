092c9cd275401b9b0bd8ad7f9aee03393e2aa396
ksbm009 <kevinstephenbiju2003@gmail.com>
2024-02-26 23:57:31 +0530

    Add user registration route

---
+++ routes/userRoute.js
@@ -0,0 +1,40 @@
+const express = require("express");
+var router = express.Router();
+
+var userModel = require('../models/userModel');
+
+// router.get('/reg',async(req,res)=>{
+//     try{
+//         var{Name,RollNo,EmailId,Password,address} = req.query;
+//         if(!EmailId){
+//             res.status(200).json({
+//                 msg:"User Email Not Found"
+//             });
+//             return;
+//         }
+//         if(!Password){
+//             res.status(200).json({
+//                 msg:"User Password Not Found"
+//             });
+//             return;
+//         }
+//         var user = new userModel();
+//         user.Name = Name;
+//         user.RollNo = RollNo;
+//         user.EmailId = EmailId;
+//         user.Password = Password;
+//         user.address = address;
+//         await user.save()
+//         res.status(200).json({
+//             msg:"Saved",
+//             data: user
+//         })
+//     }catch(e){
+//         console.log(e);
+//         res.status(500).json({
+//             msg:"Error in Saving!"
+//         })
+//     }
+// });
+
+module.exports = router;
